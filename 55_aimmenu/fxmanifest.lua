af2864831e7f4ad47c21b1588f540347c82d2ca7
mathisse2104 <mathisse.gaming@gmail.Com>
2025-05-08 17:57:07 +0200

    Add files via upload

---
+++ 55_aimmenu/fxmanifest.lua
@@ -0,0 +1,19 @@
+author "Mathisse"
+description "Weapon animation system"
+version "1.0.0"
+
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+files {
+    "data/*.lua",
+    "client/weaponanimations.meta"
+}
+
+data_file "WEAPON_ANIMATIONS_FILE" "client/weaponanimations.meta"
+
+client_scripts {
+    "@ox_lib/init.lua",
+    "client/*.lua"
+}
